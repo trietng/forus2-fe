2e51ee37e7205901b15b6efbe5b1698650c4efb3
trietng <93473943+trietng@users.noreply.github.com>
2025-01-17 21:41:15 +0700

    fix: update error handling

--- src/api/standard.ts
+++ src/api/standard.ts
@@ -22,7 +22,11 @@ standard.interceptors.request.use((config) => {
 standard.interceptors.response.use((response) => response, (error) => {
   if (error.response?.status === 401) {
     Cookies.remove('headerPayload');
-    window.location.href = '/login';
+    if (error.response?.data?.message === 'Unauthorized') {
+      window.location.href = '/login';
+    } else {
+      toast.error(error.response?.data?.message);
+    }
   }
   else {
     toast.error(error.response?.data?.message ?? 'An error occurred');
